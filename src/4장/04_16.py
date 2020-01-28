a62f84875f37ee0ffdabd55a6b101f1c1a932794
Jongkyu_homePC <wonjongkyu@naver.com>
2020-01-28 23:33:30 +0900

    파이썬을 이용한 비트코인 자동매매 - 4장 진행중

---
+++ src/4장/04_16.py
@@ -0,0 +1,5 @@
+from pandas import DataFrame
+
+data = {'opem' : [737, 750], "high": [110, 210], "low": [10, 20]}
+df = DataFrame(data)
+print(df)
---
+++ src/4장/04_18.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_excel("F:\\source\\Git\\JK_COIN_BOT\\src\\4장\\ohlc.xlsx")
+print(df)
